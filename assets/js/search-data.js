1238b99e52a169bc1fe6586216a36f4bc24baacb
mjc-ma-01 <mjc.krystal@gmail.com>
2026-04-15 07:01:32 +0000

    Deploying to gh-pages from @ mjc-ma-01/mjc-ma-01.github.io@2ad07873d2d91ad06d5c742624032c9874958dc4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -523,21 +523,14 @@ ninja.data = [{
         title: 'CV',
         section: 'Socials',
         handler: () => {
-          window.open("/assets/pdf/example_pdf.pdf", "_blank");
+          window.open("/assets/pdf/mjc_Resume.pdf", "_blank");
         },
       },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("mailto:%6D%61%6A%69%61%63%68%65%6E@%70%6A%6C%61%62.%6F%72%67.%63%6E", "_blank");
         },
       },{
         id: 'social-rss',
@@ -551,14 +544,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=aGQrFlUAAAAJ", "_blank");
         },
       },{
       id: 'light-theme',
